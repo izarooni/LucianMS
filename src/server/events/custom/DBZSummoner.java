bf6e2a3f79c0f7479c7d7e322f415585da6349ef
izarooni <coalasonly@gmail.com>
2017-08-22 19:18:29 -0600

    Shenron updates - Reverted monster removal delay - Clearing drops moved to a different timer - Whole map can be used to summon instead of a small middle area

--- src/server/events/custom/DBZSummoner.java
+++ src/server/events/custom/DBZSummoner.java
@@ -8,6 +8,7 @@ import scheduler.TaskExecutor;
 import server.life.MapleLifeFactory;
 import server.life.MapleMonster;
 import server.life.MapleNPC;
+import server.maps.MapleMap;
 import server.maps.MapleMapItem;
 import server.maps.MapleMapObject;
 import tools.MaplePacketCreator;
@@ -24,15 +25,18 @@ import java.util.Map;
 public class DBZSummoner extends GenericEvent {
 
     // platform range
-    private static final int min_x = -324;
-    private static final int max_x = 196;
+    private static final int min_x = -867;
+    private static final int max_x = 866;
     private static final int pos_y = 27;
 
+    // 7 required items to drop
     private static final int base_item = 4011009;
 
+    // Shenron NPC ID
     private static final int npcId = 9270070;
     private static final Point npcPosition = new Point(0, 50);
 
+    // Invisible monster for summon effect
     private static final int monsterId = 9500364;
     private static final Point monsterPosition = new Point(0, 36);
 
@@ -60,11 +64,13 @@ public class DBZSummoner extends GenericEvent {
     @PacketWorker
     public void onItemMove(ItemMoveHandler event) {
         MapleCharacter player = event.getClient().getPlayer();
+
+        final MapleMap map = player.getMap();
         final Point position = player.getPosition().getLocation();
         if (event.getAction() == 0) { // item drop
             Item item = player.getInventory(event.getInventoryType()).getItem(event.getSource());
             if (item.getItemId() == (base_item + balls.size())) { // dropped item is a ball
-                if (position.x >= min_x && position.x <= max_x && position.y == pos_y) {
+                if (position.x >= min_x && position.x <= max_x && position.y == pos_y) { // ball is dropped within set boundaries
                     if (balls.size() == 6) { // all balls have been dropped
 
                         player.announce(MaplePacketCreator.getClock(180));
@@ -75,7 +81,7 @@ public class DBZSummoner extends GenericEvent {
                         npc.setCy(npcPosition.y);
                         npc.setRx0(npcPosition.x - 50);
                         npc.setRx1(npcPosition.x + 50);
-                        player.getMap().addMapObject(npc);
+                        map.addMapObject(npc);
 
                         // create and position effect (monster)
                         MapleMonster monster = MapleLifeFactory.getMonster(monsterId);
@@ -91,25 +97,27 @@ public class DBZSummoner extends GenericEvent {
                         }
 
                         // currently dropping an item; we have to wait a second for the item to be added to the map's array of map-objects
-                        TaskExecutor.createTask(player.getMap()::clearDrops, 5000);
-                        TaskExecutor.createTask(new Runnable() {
-                            @Override
-                            public void run() {
-                                player.announce(MaplePacketCreator.killMonster(Integer.MAX_VALUE, false));
-                                npc.sendDestroyData(event.getClient());
-                                unregisterPlayer(player);
-                            }
-                        }, 1000 * 60 * 3);
+                        TaskExecutor.createTask(map::clearDrops, 5000);
 
                         TaskExecutor.createTask(new Runnable() {
                             @Override
                             public void run() {
-                                player.announce(MaplePacketCreator.removeNPC(npc.getObjectId()));
-                                player.getMap().removeMapObject(npc);
+                                player.announce(MaplePacketCreator.killMonster(Integer.MAX_VALUE, false)); // summon effect finished; no longer need monster
+                                player.announce(MaplePacketCreator.spawnNPC(npc)); // don't use NPC controller, otherwise NPC will not be able to de-spawn
+                                unregisterPlayer(player); // no longer need to handle packet events
+                            }
+                        }, 3100); // animation length
+
+                        // de-spawn timer
+                        TaskExecutor.createTask(new Runnable() {
+                            @Override
+                            public void run() {
+                                map.removeMapObject(npc); // remove from map to disable speaking via packet editing
+                                player.announce(MaplePacketCreator.removeNPC(npc.getObjectId())); // remove NPC
                             }
                         }, 1000 * 60 * 3);
                     } else if (item.getItemId() > base_item) { // not the first ball
-                        Point previous = balls.get(item.getItemId() - 1);
+                        Point previous = balls.get(item.getItemId() - 1); // get the position of the previous ball
                         if (position.x - previous.x > 50) { // drop from left to right
                             return; // dropped ball is too far from the previous ball location
                         }
