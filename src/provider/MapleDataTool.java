d0faac52321219688f1ff7cd3352b9e6d5c33227
izarooni <coalasonly@gmail.com>
2017-11-29 21:39:04 -0700

    Null exception fuck off

--- src/provider/MapleDataTool.java
+++ src/provider/MapleDataTool.java
@@ -49,6 +49,9 @@ public class MapleDataTool {
     }
 
     public static String getString(String path, MapleData data, String def) {
+        if (data == null) {
+            return def;
+        }
         return getString(data.getChildByPath(path), def);
     }
 
