8e83cee4754b82b61c0fef021c2c42677090115a
izarooni <coalasonly@gmail.com>
2018-05-03 09:24:56 -0700

    Added Timeless Gear trader

---
+++ scripts/npc/world0/f_timeless_trader.js
@@ -0,0 +1,15 @@
+load("scripts/npc/generic_shop.js");
+
+var pointsType = Java.type("constants.ServerConstants").CURRENCY;
+
+try {
+    var file = new java.io.File("resources/data-timeless.json");
+    var content = Packages.org.apache.commons.io.FileUtils.readFileToString(file);
+    var json = JSON.parse(content);
+
+    for (var j in json) {
+        items[j] = JSON.parse("[" + json[j] + "]");
+    }
+} catch (e) {
+    broken = e.message.replace(/\\/g, "/");
+}
