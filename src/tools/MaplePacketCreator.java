a46157c8b35fab6c3b84e2be6d13bb9b25d68763
izarooni <coalasonly@gmail.com>
2018-05-12 16:32:33 -0600

    Spouse chat message changes

--- src/tools/MaplePacketCreator.java
+++ src/tools/MaplePacketCreator.java
@@ -26,8 +26,6 @@ import client.inventory.*;
 import client.inventory.Equip.ScrollResult;
 import client.status.MonsterStatus;
 import client.status.MonsterStatusEffect;
-import com.lucianms.server.pqs.carnival.MCarnivalGame;
-import com.lucianms.server.pqs.carnival.MCarnivalTeam;
 import constants.GameConstants;
 import constants.ItemConstants;
 import constants.ServerConstants;
@@ -57,6 +55,8 @@ import server.life.MapleNPC;
 import server.life.MobSkill;
 import server.maps.*;
 import server.movement.LifeMovementFragment;
+import com.lucianms.server.pqs.carnival.MCarnivalGame;
+import com.lucianms.server.pqs.carnival.MCarnivalTeam;
 import tools.data.output.LittleEndianWriter;
 import tools.data.output.MaplePacketLittleEndianWriter;
 
@@ -6286,6 +6286,7 @@ public class MaplePacketCreator {
             mplew.writeInt(mPartnerId);
             mplew.writeInt(fPartnerId);
             mplew.writeShort(3); // married ? 3 : 1
+            // if marriage, wedding rings -- otherwise engagement box & ring
             mplew.writeInt(ring.getItemId());
             mplew.writeInt(ring.getItemId());
             mplew.writeAsciiString(StringUtil.getRightPaddedStr(MapleCharacter.getNameById(mPartnerId), '\0', 13));
