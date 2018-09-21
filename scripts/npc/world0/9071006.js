f289870971bf1e6dcd9cb76c0ad1569a393cb095
Ian <coalasonly@gmail.com>
2018-09-21 15:30:24 -0700

    Fixed an issue with Monster Park entrance portals

--- scripts/npc/world0/9071006.js
+++ scripts/npc/world0/9071006.js
@@ -1,6 +1,5 @@
 /* izarooni */
 // npc: 9071000
-var MonsterPark = Java.type("com.lucianms.features.MonsterPark");
 var status = 0;
 
 function action(mode, type, selection) {
@@ -11,30 +10,7 @@ function action(mode, type, selection) {
         status++;
     }
     if (status == 1) {
-        if (cm.getParty() == null) {
-            cm.sendOk("So you want to enter the Monster Park? Well I can't let you enter without a party");
-            cm.dispose();
-        } else if (cm.isLeader()) {
-             cm.sendNext("Whenever you're ready you may enter");
-        } else {
-            cm.sendOk("Only your party leader can decide when to enter");
-            cm.dispose();
-        }
-    } else if (status == 2) {
-        this.optional = player.getGenericEvents().stream().filter(function(e){
-            return e instanceof MonsterPark;
-        }).findFirst();
-        if (optional.isPresent()) {
-            cm.sendYesNo("You are already registered in a Monster Park!\r\nWould you like to leave?");
-        } else {
-            var park = new MonsterPark(client.getWorld(), client.getChannel(), 953050000);
-            park.registerPlayer(player);
-            cm.dispose();
-        }
-    } else if (status == 3) {
-        if (this.optional.isPresent()) {
-            this.optional.get().unregisterPlayer(player);
-            cm.dispose();
-        }
+        cm.sendOk("Are you ready for Monster Park? Alone or with a party, when you're ready just enter through a gate over there!");
+        cm.dispose();
     }
 }
