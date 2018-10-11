2b37a6c9787f8207b244a77756e220ae5598b761
LucianVPS <lucian@ms.com>
2018-10-11 06:42:37 -0700

    Text changes to Noragami NPCs

--- scripts/npc/world0/9899949.js
+++ scripts/npc/world0/9899949.js
@@ -30,20 +30,23 @@ function action(mode, type, selection) {
     if (status == 0) { // pre-quest status
         cm.sendOk("I don't need help. I'm very busy right now.");
         cm.dispose();
-    } else if (status == 1) {
+    } else if (status == 1) { // quest begining
         cm.sendNext("Placeholder 1");
     } else if (status == 2) {
         cm.sendNext("Placeholder 2");
-    } else if (status == 3) {
+    } else if (status == 3) { // quest progress
         DisplaySummary(metadata);
     } else if (status == 4) {
         cm.sendAcceptDecline("uwu");
     } else if (status == 5) {
         CQuests.beginQuest(player, QuestID);
         cm.dispose();
-    } else if (status == 6) {
-        cm.sendOk("Complete!");
-        quest.complete(player);
+    } else if (status == 6) { // quest complete
+        if (quest.complete(player)) {
+            cm.sendOk("Complete!");
+        } else {
+            cm.sendOk("Check if your inventory is full, you must accept my rewards!");
+        }
         cm.dispose();
     } else cm.dispose();
 }
--- scripts/npc/world0/9899950.js
+++ scripts/npc/world0/9899950.js
@@ -30,18 +30,18 @@ function action(mode, type, selection) {
     if (status == 0) { // pre-quest status
         cm.sendOk("I don't need help. I'm very busy right now.");
         cm.dispose();
-    } else if (status == 1) {
+    } else if (status == 1) { // quest beginning
         cm.sendNext("Placeholder 1");
     } else if (status == 2) {
         cm.sendNext("Placeholder 2");
-    } else if (status == 3) {
+    } else if (status == 3) { // quest progress
         DisplaySummary(metadata);
     } else if (status == 4) {
         cm.sendAcceptDecline("uwu");
     } else if (status == 5) {
         CQuests.beginQuest(player, QuestID);
         cm.dispose();
-    } else if (status == 6) {
+    } else if (status == 6) { // complete
         cm.sendOk("Complete!");
         quest.complete(player);
         cm.dispose();
