e5c274f5bce6ef25fd3850e849760bb2e61aed59
Ian <coalasonly@gmail.com>
2018-10-14 05:17:11 -0600

    Fixed an issue where Nobless or Aran beginner are unable to job advance

--- scripts/npc/generic_jobadvance.js
+++ scripts/npc/generic_jobadvance.js
@@ -24,7 +24,8 @@ function action(mode, type, selection) {
     let jobID = player.getJob().getId();
     let advancement = (jobID % 100 % 10);
 
-    if (jobID != 0  && !containsJob(jobs, jobID)) {
+    if (jobID != 0 && jobID != 1000 && jobID == 2000
+          && !containsJob(jobs, jobID)) {
         cm.sendOk("This path is not meant for you now.");
         cm.dispose();
         return;
