c853ee4a7f7b69c52866fd669e4c8cc31846272a
LucianVPS <lucianms@vps.com>
2018-10-16 12:29:12 -0400

    Halloween Candy trader

--- scripts/npc/world0/9201142.js
+++ scripts/npc/world0/9201142.js
@@ -2,9 +2,9 @@ load("scripts/npc/generic_shop.js");
 
 const pointsType = 4031203;
 
-dialog.intro = "Happy halloween!";
+dialog.intro = "Happy #rHalloween#k! Do you have candies for me? You can find them by killing monsters.";
 dialog.first = "#b{PointsName}#k are good. Trade them now!\r\n You currently have #b{PointsQuantity} {PointsName}#l";
-dialog.second = "some text";
+dialog.second = "Are you ready to spend some candy?";
 
 try {
     let file = new java.io.File("resources/data-candy.json");
