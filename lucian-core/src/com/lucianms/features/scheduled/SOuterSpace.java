ebcdd8bcf7c338ecc3c1f8da5735625c4d51d880
Ian <coalasonly@gmail.com>
2019-03-04 23:16:26 -0700

    Changed message announcement text for Space Slime summoning

--- lucian-core/src/com/lucianms/features/scheduled/SOuterSpace.java
+++ lucian-core/src/com/lucianms/features/scheduled/SOuterSpace.java
@@ -99,7 +99,7 @@ public class SOuterSpace extends SAutoEvent {
         setOpen(true);
         start = System.currentTimeMillis();
         mapInstances.clear();
-        world.broadcastMessage(0, "The Space Slime has spawned in the Outer Space, Planet Lucian");
+        world.broadcastMessage(0, "The Space Slime has spawned in Outer Space, Planet Lucian");
         TaskExecutor.createTask(this::end, 1000 * 60 * 5);
     }
 
