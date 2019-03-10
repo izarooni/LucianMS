f58f4d20c32c1fe12ca3713df97f286f90816834
Ian <coalasonly@gmail.com>
2019-03-09 18:59:17 -0700

    A null exception occurring when using a pet command

--- lucian-channel/src/com/lucianms/events/PlayerPetChatEvent.java
+++ lucian-channel/src/com/lucianms/events/PlayerPetChatEvent.java
@@ -2,7 +2,6 @@ package com.lucianms.events;
 
 import com.lucianms.client.MapleCharacter;
 import com.lucianms.nio.receive.MaplePacketReader;
-import com.lucianms.events.PacketEvent;
 import tools.MaplePacketCreator;
 
 /**
@@ -20,7 +19,7 @@ public class PlayerPetChatEvent extends PacketEvent {
         reader.readInt();
         reader.readByte();
         action = reader.readByte();
-        String content = reader.readMapleAsciiString();
+        content = reader.readMapleAsciiString();
         if (petID < 0 || petID > 3 || action < 0 || action > 9) {
             setCanceled(true);
         }
