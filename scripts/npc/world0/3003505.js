c7dffe7230788116c8b75fe2714867bdab44a22a
Ian <coalasonly@gmail.com>
2019-07-02 11:05:15 -0600

    Fixed NPC for giving Mask of Truth (3003505)

--- scripts/npc/world0/3003505.js
+++ scripts/npc/world0/3003505.js
@@ -9,12 +9,12 @@ function action(mode, type, selection) {
         status++;
     }
     if (status == 1) {
-		cm.sendSimple("You defeated me. I suppose you are worth enough to see the world for what it is. Take this mask and find out the truth between light and darkness. \r\n#L0##i1012299##bTake Mask#k");
+        cm.sendSimple("You defeated me. I suppose you are worth enough to see the world for what it is. Take this mask and find out the truth between light and darkness. \r\n#L0##i1012299##bTake Mask#k");
     } else if (status == 2) {
-		if (Selection == 0) {
-			cm.gainItem(1012299, 1);
-			cm.warp(86, 0);
-		}
-		cm.dispose();
-	}
+        if (selection == 0) {
+            cm.gainItem(1012299, 1);
+            cm.warp(86, 0);
+        }
+        cm.dispose();
+    }
 }
