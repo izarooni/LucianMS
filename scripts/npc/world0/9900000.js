0b4dc99512867132a0a317d65e522bcc8b3ce8aa
Ian <coalasonly@gmail.com>
2019-07-02 10:59:40 -0600

    Job change is free for 0RBs, 3RB points otherwise

--- scripts/npc/world0/9900000.js
+++ scripts/npc/world0/9900000.js
@@ -2,12 +2,12 @@ const Jobs = [
     new Job("\r\n\t\t\t\t\t\t\t\t", "#eHero#k - Uchiha", 112),
     new Job("\r\n\t\t\t\t\t\t", "#ePaladin#k - Rashoumon", 122),
     new Job("\r\n\t\t\t\t\t\t", "#eDark Knight#k - Dragoon", 132),
-    new Job("\r\n\t\t\t\t\t   ", "#eDawn Warrior#k - Valkryie\r\n", 1111),
+    new Job("\r\n\t\t\t\t\t   ", "#eDawn Warrior#k - Valkyrie\r\n", 1111),
     new Job("\r\n\t\t\t\t", "#eArchmage (Fire/Poison)#k - Cadena", 212),
     new Job("\r\n\t\t\t", "#eArchmage (Ice/Lightning)#k - Luminous", 222),
     new Job("\r\n\t\t\t\t\t\t\t\t", "#eBishop#k - Evan\r\n", 232),
     new Job("\r\n\t\t\t\t\t   ", "#eBowmaster#k - PathFinder", 312),
-    new Job("\r\n\t\t\t\t\t\t", "#eMarksman#k - Marksman\r\n", 322),
+    new Job("\r\n\t\t\t\t\t\t", "#eMarksman#k - Wildhunter\r\n", 322),
     new Job("\r\n\t\t\t\t\t\t\t", "#eNight Lord#k - Dancer", 412),
     new Job("\r\n\t\t\t\t\t\t", "#eShadower#k - Dual Blader\r\n", 422),
     new Job("\r\n\t\t\t\t\t\t\t", "#eBuccaneer#k - Ark", 512),
@@ -21,21 +21,30 @@ function action(mode, type, selection) {
         return cm.dispose();
     }
     status++;
+
+    let cost = player.getRebirths() == 0 ? 0 : 3;
+
     if (status == 1) {
         let content = "\t#dYou view me anytime using the < @jobs > command!\r\n\r\n";
         content += "\t\t\t\t\t\t\t\t\t\t\t #bJob List#k\r\n";
-        content += "\t\t\t\tIt costs #b12 rebirth points#k to change jobs\r\n";
+        if (cost == 0) {
+            content += "\t  You may freely change your job until your first rebirth!\r\n";
+        } else {
+            content += `\t\t\t\tIt costs #b${cost} rebirth points#k to change jobs\r\n`;
+        }
         for (let i = 0; i < Jobs.length; i++) {
             let job = Jobs[i];
             content += `${job.format}#L${i}#${job.text}#l`;
         }
         cm.sendSimple(content);
     } else if (status == 2) {
-        if (player.getRebirthPoints() >= 12) {
+        if (player.getRebirthPoints() >= cost) {
             let job = Jobs[selection];
             player.changeJob(Packages.com.lucianms.client.MapleJob.getById(job.jobID));
-            player.setRebirthPoints(player.getRebirthPoints() - 12);
-            player.sendMessage("You now have {} rebirth points", player.getRebirthPoints());
+            if (cost > 0) {
+                player.setRebirthPoints(player.getRebirthPoints() - cost);
+                player.sendMessage("You now have {} rebirth points", player.getRebirthPoints());
+            }
         } else {
             cm.sendOk("You do not have enough rebirth points");
         }
