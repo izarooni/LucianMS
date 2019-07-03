0ab16bff5fa7fa95e3ad483a44cc4ea6022c22bd
Ian <coalasonly@gmail.com>
2019-07-03 12:07:01 -0600

    Auto-rebirth reactors now warp when item is gained

--- scripts/reactor/1002002.js
+++ scripts/reactor/1002002.js
@@ -5,8 +5,14 @@ function hit() {
     if (reactor.getMap().getId() != 89) return;
 
     let state = reactor.getState();
-    if (state == 4) { // last visible state in WZ
-        rm.dropItem(ServerConstants.getAutoRebirthItem(), 1);
+    if (state == 2) { // last visible state in WZ
+        if (Math.random() < 0.13) {
+            rm.getPlayer().sendMessage(-1, "A small shimmering object falls out of the box...");
+            rm.gainItem(ServerConstants.getAutoRebirthItem(), 1, true);
+            rm.getPlayer().changeMap(reactor.getMap().getId(), 30);
+        } else {
+            rm.getPlayer().sendMessage(-1, "The box was destroyed but nothing was found inside...");
+        }
         reactor.getMap().sendPacket(reactor.makeDestroyData());
         reactor.setAlive(false);
     }
