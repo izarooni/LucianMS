7dd338280860ef1fe3c0c15a00acb7d49583fe08
Ian <coalasonly@gmail.com>
2019-07-04 09:06:56 -0600

    NPC for entering Datascape map once per day

--- scripts/npc/world0/9899955.js
+++ scripts/npc/world0/9899955.js
@@ -1,8 +1,34 @@
-/*
- * @Author:     Venem
- * Map:     Plaza final NPC
-*/
-function start() {
-    cm.sendOk("So you really wanna test me?\r\n\r\nOkay..So be it.\r\n\r\nYou're nothing and your faith is all sealed. No one escapes my #rPLAZA#K\r\n\r\n                                 #rPREPARE TO DIE!#k");
-    cm.dispose();
+load('scripts/util_imports.js');
+const EntryLimits = Java.type("tools.EntryLimits");
+const ENTRY_TYPE = "auto_rb";
+const ONE_DAY = (1000 * 60 * 60 * 24);
+/* izarooni */
+let status = 0;
+
+function action(mode, type, selection) {
+    if (mode < 1) {
+        cm.dispose();
+        return;
+    } else {
+        status++;
+    }
+    if (status == 1) {
+        let entry = EntryLimits.getEntries(player.getId(), ENTRY_TYPE);
+        if (entry != null) {
+            if (entry.LastEntry + ONE_DAY  < Date.now() || player.isDebug()) {
+                EntryLimits.reset(player.getId(), ENTRY_TYPE);
+            } else {
+                cm.sendOk("You can only enter the Datascape once per day."
+                 + `\r\nCome back in #b${StringUtil.getTimeElapse((entry.LastEntry + ONE_DAY) - Date.now())}#k.`);
+                return cm.dispose();
+            }
+        }
+        cm.sendNext("Are you ready to enter the Datascape?"
+            + `\r\nInside you will break boxes around the map and try to obtain #b#z${ServerConstants.getAutoRebirthItem()}##k`
+            + "\r\n");
+    } else if (status == 2) {
+        EntryLimits.incrementEntry(player.getId(), ENTRY_TYPE);
+        player.changeMap(89);
+        cm.dispose();
+    }
 }
